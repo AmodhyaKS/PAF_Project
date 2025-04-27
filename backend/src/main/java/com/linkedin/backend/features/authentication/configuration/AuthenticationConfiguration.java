125bddc86d9c792d4c1cd79076f5c893624ab0bd
it22599568 <it22599568@my.sliit.lk>
2025-04-27 10:36:50 +0530

    user login

--- backend/src/main/java/com/linkedin/backend/features/authentication/configuration/AuthenticationConfiguration.java
+++ backend/src/main/java/com/linkedin/backend/features/authentication/configuration/AuthenticationConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 
+
+
 @Configuration
 public class AuthenticationConfiguration {
     @Bean
